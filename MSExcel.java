8bded4b17ca3943e6ce3c1b59e067fdaa99aac41
Virgilio Gatchalian <encoder234@gmail.com>
2025-12-05 07:22:01 +0800

    M2 Activity 4

---
+++ MSExcel.java
@@ -0,0 +1,19 @@
+package module2.activity4;
+
+public class MSExcel implements Executable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("Opening MS Excel...");
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		System.out.println("Stopping MS Excel...");
+	}
+
+	
+	
+}
---
+++ MSWord.java
@@ -0,0 +1,17 @@
+package module2.activity4;
+
+public class MSWord implements Executable{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("Opening MS Word...");
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		System.out.println("Stopping MS Word...");
+	}
+
+}
