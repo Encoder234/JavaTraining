5eedacbf6cd3847fee0f55356ecb28648f14562a
Virgilio Gatchalian <encoder234@gmail.com>
2025-12-04 16:26:10 +0800

    M2 Activity 1

---
+++ House.java
@@ -0,0 +1,12 @@
+package module2.activity1;
+
+public class House {
+
+	String address = "Japan";
+	
+	public String return_address() {
+		return this.address;
+	}
+	
+	
+}
---
+++ Tree.java
@@ -0,0 +1,14 @@
+package module2.activity1;
+
+public class Tree {
+
+	String type = "Banana";
+	String height = "100m";
+	String color = "Green";
+	
+	public String return_height() {
+		return this.height;
+	}
+	
+	
+}
