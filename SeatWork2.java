80c47c168d5bd2d1f087b8e618a5931f938b1c86
Virgilio Gatchalian <encoder234@gmail.com>
2025-12-04 16:14:28 +0800

    M2 Activity 2

---
+++ SeatWork2.java
@@ -0,0 +1,14 @@
+package module2.activity2;
+
+public class SeatWork2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		WhiteBoard wb = new WhiteBoard();
+		int sum = wb.addNumbers(5, 3);
+		System.out.println("the total is: " + String.valueOf(sum));
+		
+	}
+
+}
